445a036427bf816d88cd00af678d9bf7aaf65558
Mohamed-Angular <stockageinfo2020@gmail.com>
2021-12-12 17:37:06 +0100

    Modifs V4

--- DictionnaireProject.DataAccess/Repositories/DataRepository.cs
+++ DictionnaireProject.DataAccess/Repositories/DataRepository.cs
@@ -1,42 +1,42 @@
-﻿ 
+﻿
 using System;
- 
+
 using System.Threading.Tasks;
 using System.Net.Http;
 using Newtonsoft.Json;
-using DictionnaireProject.DataAccess.Constants.Api; 
+using DictionnaireProject.DataAccess.Constants.Api;
 
 using System.Net;
 using DictionnaireProject.DataAccess.Interfaces;
 using DictionnaireProject.DataAccess.Exceptions;
 using DictionnaireProject.DataAccess.Exceptions.Constants;
- 
+
 
 namespace DictionnaireProject.DataAccess.Repositories
 {
-   public class DataRepository : IApiRepository
+    public class DataRepository : IApiRepository
     {
-        public string apiResponse { get; private set; }
+
         public async Task<object> GetWith1Input(string keyFirst)
         {
             try
             {
-                
 
-                 string url = Apis.ApiUrlData;
+                string apiResponse;
+                string url = Apis.ApiUrlData;
                 using (var httpClient = new HttpClient())
                 {
                     using (var response = await httpClient.GetAsync(url + keyFirst))
                     {
-                          apiResponse = await response.Content.ReadAsStringAsync();
-                      
+                        apiResponse = await response.Content.ReadAsStringAsync();
+
                     }
                 }
                 return apiResponse;
             }
             catch (Exception ex)
             {
-                 throw new TechnicalException(MessagesConstantes.MessageErrorReading, HttpStatusCode.BadRequest);
+                throw new TechnicalException(MessagesConstantes.MessageErrorReading, HttpStatusCode.BadRequest);
             }
         }
     }
